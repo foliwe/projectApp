214e9e22aa0656cb911a199cdefdfec216a33ab3
foliwe <foliwe@gmail.com>
2018-05-08 19:02:02 +0100

    Fixed conflict3

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root to: 'dashboard#index'
 
-<<<<<<< HEAD
   resources :employees
   resources :invoices
   resources :projects do
@@ -9,16 +8,6 @@ Rails.application.routes.draw do
      resources :expenses
 
   end
-=======
-  get 'projects/index'
-
-  root 'projects#index'
-
-  resources :projects 
-  resources :tasks
-  resources :expenses
-  resources :invoices
   
->>>>>>> 2ccbb8628ee8fbf8966c0af8515bdc9f52cc3d46
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
